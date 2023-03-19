f88d88533fdd5e3d9d56f844b3cc528fc34fccf5
Solarev Ruslan <ruslan251207@gmail.com>
2023-03-19 16:45:37 +1000

    finished version

--- news/models.py
+++ news/models.py
@@ -5,10 +5,13 @@ class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
-    data = models.DateTimeField('Дата публикации')
+    date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
     
     class Meta:
         verbose_name = 'Новость'
